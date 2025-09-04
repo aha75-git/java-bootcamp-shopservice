021ced26a510328c7946587abbb8342df7626b18
lazio <ahaffner75@gmail.com>
2025-09-04 12:20:49 +0200

    import entfernt.

--- src/main/java/bootcamp/shopservice/order/Order.java
+++ src/main/java/bootcamp/shopservice/order/Order.java
@@ -3,7 +3,6 @@ package bootcamp.shopservice.order;
 import bootcamp.shopservice.product.Product;
 
 import java.math.BigDecimal;
-import java.util.List;
 import java.util.Map;
 
 public record Order(String orderId, Map<Product, Integer> mapProductQuantity) {
